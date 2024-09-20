c7509dfaae0305d642ec1baed08215d54f79642c
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-20 19:49:50 +0300

    Refactor FeaturedPosts component and update Tailwind CSS configuration

--- components/Home/FeaturedPosts.tsx
+++ components/Home/FeaturedPosts.tsx
@@ -11,20 +11,23 @@ interface FeaturedPostsProps {
 const FeaturedPosts = ({ posts }: FeaturedPostsProps) => {
   const sortedBlogs = sortPosts(posts);
   return (
-    <section className="w-full mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center">
-      <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">Featured Posts</h2>
-      <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-10 sm:mt-16">
-        <article className="col-span-2 sxl:col-span-1 row-span-2 relative">
-          <BlogLayoutOne post={sortedBlogs[1]} />
-        </article>
-        <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+  <section className="w-full mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col items-center justify-center">
+    <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">Featured Posts</h2>
+
+    <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
+      <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
+        <BlogLayoutOne post={sortedBlogs[1]} />
+      </article>
+      <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
           <BlogLayoutTwo post={sortedBlogs[2]} />
-        </article>
-        <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+
+      </article>
+      <article className="col-span-2 sm:col-span-1 row-span-1 relative">
           <BlogLayoutTwo post={sortedBlogs[3]} />
-        </article>
-      </div>
-    </section>
+
+      </article>
+    </div>
+  </section>
   );
 };
 
