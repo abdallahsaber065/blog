7b8ae2c1f47df5930412f474a1f7505e7a800fdc
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-23 00:10:57 +0300

    Refactor image paths in content files and update dependencies

--- next.config.js
+++ next.config.js
@@ -2,10 +2,27 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.alias['public'] = path.join(__dirname, 'public');
-        return config;
-    },
+  webpack: (config) => {
+    config.resolve.alias['public'] = path.join(__dirname, 'public');
+    return config;
+  },
 };
 
-module.exports = nextConfig;
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+  remarkPlugins: [require('remark-gfm')],
+  rehypePlugins: [
+    require('rehype-slug'),
+    require('rehype-autolink-headings'),
+    require('rehype-pretty-code'),
+  ],
+  // MDX-specific options can go here
+  },
+});
+
+module.exports = withMDX({
+  ...nextConfig, // Merge nextConfig into withMDX options
+  // Next.js config options here
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'], // Add MD and MDX as page extensions
+});
