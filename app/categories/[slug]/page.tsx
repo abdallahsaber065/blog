952c5e47f792b7b1229919e8c6654b856a7b3b09
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-22 15:41:06 +0300

    Refactor CategoryPage component and update blog post rendering logic

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -68,9 +68,9 @@ const CategoryPage = async ({ params }: { params: { slug: string } }) => {
       <Categories categories={allCategories} currentSlug={params.slug} />
 
       <div className="grid  grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 grid-rows-2 gap-16 mt-5 sm:mt-10 md:mt-24 sxl:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32">
-        {filteredBlogs.map((blog, index) => (
+        {filteredBlogs.map((post, index) => (
           <article key={index} className="col-span-1 row-span-1 relative">
-            <BlogLayoutThree post={blog} />
+            <BlogLayoutThree post={post} />
           </article>
         ))}
       </div>
