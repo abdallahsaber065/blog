13d21d2663f3cde5789259855ca15289d7de529f
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-23 00:10:57 +0300

    Refactor image paths in content files and update dependencies

--- components/Blog/RenderMdx.tsx
+++ components/Blog/RenderMdx.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React from 'react'
-import { MDXProvider } from '@mdx-js/react'
 import Image from 'next/image'
 import { PrismaClient, Post } from '@prisma/client'
 
@@ -11,36 +10,55 @@ const mdxComponents = {
 }
 
 const RenderMdx = ({ post }: { post: Post }) => {
-    console.log('Post content:', post.content)
-    const MDXContent = useMDXComponent(post.content)
 
-    try {
-        return (
-            <div className='col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
-            prose-blockquote:bg-accent/20 
-            prose-blockquote:p-2
-            prose-blockquote:px-6
-            prose-blockquote:border-accent
-            prose-blockquote:not-italic
-            prose-blockquote:rounded-r-lg
 
-            prose-li:marker:text-accent
 
-            dark:prose-invert
-            dark:prose-blockquote:border-accentDark
-            dark:prose-blockquote:bg-accentDark/20
-            dark:prose-li:marker:text-accentDark
+    return (
+        <div className='col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
+        prose-blockquote:bg-accent/20 
+        prose-blockquote:p-2
+        prose-blockquote:px-6
+        prose-blockquote:border-accent
+        prose-blockquote:not-italic
+        prose-blockquote:rounded-r-lg
 
-            first-letter:text-3xl
-            sm:first-letter:text-5xl
-            '> 
-                <MDXContent components={mdxComponents} />
-            </div>
-        )
-    } catch (error) {
-        console.error('Error rendering MDX content:', error)
-        return <div>Error rendering content</div>
-    }
+        prose-li:marker:text-accent
+
+        dark:prose-invert
+        dark:prose-blockquote:border-accentDark
+        dark:prose-blockquote:bg-accentDark/20
+        dark:prose-li:marker:text-accentDark
+
+        first-letter:text-3xl
+        sm:first-letter:text-5xl
+        '> 
+
+        </div>
+    )
+}
+
+export async function getServerSideProps(context: any) {
+  const { slug }: { slug: string } = context.params;
+  const post: Post | null = await prisma.post.findUnique({
+    where: { slug },
+    include: {
+      author: true,
+      tags: true,
+      category: true,
+    },
+  });
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post,
+    },
+  };
 }
 
 export default RenderMdx
