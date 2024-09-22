61163d3e28a88acfe82c1c635e2015f4e1288c9e
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-22 21:38:31 +0300

    Refactor image paths in content files and update dependencies

---
+++ mdx-components.tsx
@@ -0,0 +1,9 @@
+import type { MDXComponents } from 'mdx/types'
+// use globals.css to import the global styles
+import 'styles/globals.css'
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+    return {
+        ...components,
+    }
+}
