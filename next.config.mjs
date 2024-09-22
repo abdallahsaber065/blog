6eda603b5a00c2bfbe89c53674493d6382cb6744
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-23 01:43:04 +0300

    Refactor image paths in content files and add next-mdx-remote dependency

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+import remarkGfm from 'remark-gfm';
+import createMDX from '@next/mdx';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['public'] = path.join(__dirname, 'public');
+    return config;
+  },
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+// Wrap MDX and Next.js config with each other
+export default withMDX(nextConfig);
