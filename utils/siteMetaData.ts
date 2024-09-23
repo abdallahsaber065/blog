9d802b44c50965a704df3f969793bde3fc936eae
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-24 01:35:38 +0300

    Add browserconfig.xml and site.webmanifest for favicon and app icons

--- utils/siteMetaData.ts
+++ utils/siteMetaData.ts
@@ -1,22 +1,21 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
-    language: 'en-us',
-    theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
-    siteLogo: '/logo.png',
-    socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
-    dribbble: 'https://www.dribbble.com',
-    locale: 'en-US',
-  }
-  
+  title: 'Next.js Blog With Tailwind CSS, Prisma ORM and Postgres',
+  author: 'Abdallah Saber',
+  headerTitle: 'Dev Trend Blog',
+  description: ' Dev Trend is your go-to source for the latest in web development, programming, and tech news. Our mission is to empower developers and tech enthusiasts with insightful articles, tutorials, and resources.',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'abdallahsaber065@gmail.com',
+  github: 'https://github.com/abdallah065',
+  twitter: 'https://x.com/DevTrend0',
+  facebook: 'https://www.facebook.com/devtrends',
+  linkedin: 'https://www.linkedin.com/in/abdallah-saber065/',
+  dribbble: 'https://twitter.com/DevTrend0',
+  locale: 'en-US',
+}
+
 export default siteMetadata;
