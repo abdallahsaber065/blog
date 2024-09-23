dcc27e927287d4b4c455fbff8bfd10c04213efba
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-23 21:32:55 +0300

    Refactor RenderMdx component and mdxOptions.js

--- components/Blog/RenderMdx.tsx
+++ components/Blog/RenderMdx.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import Image from 'next/image';
 import { Post as PrismaPost, Tag, User } from '@prisma/client';
-import { MDXRemoteSerializeResult, SerializeOptions } from 'next-mdx-remote/dist/types';
-import mdxOptions from '@/mdxOptions'; // Adjust the path accordingly
-
+import { SerializeOptions } from 'next-mdx-remote/dist/types';
+// mdxOptions.js
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 type PostWithAuthorAndTags = PrismaPost & {
   author: User | null;
@@ -24,8 +27,19 @@ const mdxComponents = (featuredImageUrl: string) => ({
   ),
 });
 
-const RenderMdx = ({ post }: { post: PostWithAuthorAndTags}) => {
-  // console.log(mdxSource);
+const RenderMdx = ({ post }: { post: PostWithAuthorAndTags }) => {
+
+  const mdxOptions = {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "wrap" }],
+      rehypePrettyCode,
+    ],
+  };
+
+  console.log('MDX Options:', mdxOptions);
+
   return (
     <div className='col-span-12 lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
     prose-blockquote:bg-accent/20 
@@ -45,11 +59,13 @@ const RenderMdx = ({ post }: { post: PostWithAuthorAndTags}) => {
     first-letter:text-3xl
     sm:first-letter:text-5xl
     '>
-      <MDXRemote
-        source={post.content}
-        components={mdxComponents(post.featured_image_url || '')}
-        options={mdxOptions as SerializeOptions}
-      />
+      <Suspense fallback={<>Loading...</>}>
+        <MDXRemote
+          source={post.content}
+          components={mdxComponents(post.featured_image_url || '')}
+          options={mdxOptions as SerializeOptions}
+        />
+      </Suspense>
     </div>
   );
 };
