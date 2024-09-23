4d7dc14a63dc3b256c21edd271381fb889d07cbd
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-24 01:43:02 +0300

    Refactor code to remove unused Dribbble link and update footer copyright year

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -68,20 +68,11 @@ const Footer = () => {
         >
           <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
         </a>
-        <a
-          href={siteMetadata.dribbble}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Check my profile on Dribbble"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
       </div>
 
       <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
         <span className="text-center">
-          &copy;2023 CodeBucks. All rights reserved.
+          &copy;2024 Dev Trend. All rights reserved.
         </span>
         <Link
           href="/sitemap.xml"
@@ -91,8 +82,8 @@ const Footer = () => {
         </Link>
         <div className="text-center">
           Made with &hearts; by{" "}
-          <a href="https://devdreaming.com" className="underline" target="_blank">
-            CodeBucks
+          <a href="https://abdallah-saber.vercel.app/" className="underline" target="_blank">
+            Abdallah Saber
           </a>
         </div>
       </div>
