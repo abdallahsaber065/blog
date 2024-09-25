300cc355000c7f651d578648d93080ae61e406b8
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-26 00:21:44 +0300

    Refactor HomeCoverSection component styles

--- components/Home/HomeCoverSection.tsx
+++ components/Home/HomeCoverSection.tsx
@@ -21,18 +21,18 @@ const HomeCoverSection = ({ posts }: HomeCoverSectionProps) => {
   return (
     <div className='w-full inline-block'>
       <article className='flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[60vh] sm:h-[85vh]'>
-        <div className='absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0' />
+        <div className='absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-10' />
         <Image
           src={post.featured_image_url || '/default-image.jpg'}
           placeholder='blur'
           blurDataURL={post.featured_image_url || '/default-image.jpg'}
           alt={post.title}
           fill
-          className='w-full h-full object-center object-cover rounded-3xl -z-10'
+          className='w-full h-full object-center object-cover rounded-3xl z-0'
           sizes='100vw'
           priority
         />
-        <div className='w-full lg:w-3/4 p-6 sm:p-8 md:p-12 lg:p-16 flex flex-col items-start justify-center z-0 text-light'>
+        <div className='w-full lg:w-3/4 p-6 sm:p-8 md:p-12 lg:p-16 flex flex-col items-start justify-center z-20 text-light'>
           <Tag link={`/categories/${slug(post.tags[0].name)}`} name={post.tags[0].name} />
           <Link href={`/blogs/${post.slug}`} className='mt-6'>
             <h1 className='font-bold capitalize text-lg sm:text-xl md:text-3xl lg:text-4xl'>
