e0dfbffb415a647ca00ecaef488f3eb604db5f86
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-26 18:36:56 +0300

    feat(layout): move favicon and manifest links to RootLayout for better structure

--- pages/_layout.tsx
+++ pages/_layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import siteMetadata from "@/utils/siteMetaData";
 import { ReactNode } from 'react';
+import Head from 'next/head';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,7 +18,6 @@ const manrope = Manrope({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-mr",
-
 });
 
 export const metadata = {
@@ -64,6 +64,21 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         "font-mr bg-light dark:bg-dark"
       )}
     >
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/static/images/icons/favicon.ico" />
+        <link rel="apple-touch-icon" href="/static/images/icons/apple-touch-icon.png" />
+        {/* android-chrome */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/static/images/icons/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/static/images/icons/android-chrome-512x512.png" />
+        {/* favicon 16 and 32 */}
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/images/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/images/icons/favicon-32x32.png" />
+        {/* safari-pinned-tab */}
+        <link rel="mask-icon" href="/static/images/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        {/* mstile 150x150 */}
+        <meta name="msapplication-TileImage" content="/static/images/icons/mstile-150x150.png" />
+      </Head>
       <Header />
       {children}
       <Footer />
