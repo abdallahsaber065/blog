e210d938d99d8b9c1199b6a36cec8830c44b3308
Abdallah Saber <abdallahsaber065@gmail.com>
2024-09-28 03:33:28 +0300

    feat(api): add limit parameter support for posts API query

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -46,8 +46,8 @@ module.exports = {
   ],
 
   daisyui: {
-    themes: ["light", "dark", "bumblebee", "cmyk", "sunset", "night", "dim"], // active themes
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    // themes: ["light", "dark", "bumblebee", "cmyk", "sunset", "night", "dim"], // active themes
+    // darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
