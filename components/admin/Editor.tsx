a35f6ed5c1fe7b999998c9bddaecf504878d9d29
Abdallah Saber <abdallahsaber065@gmail.com>
2024-10-01 19:31:18 +0300

    feat(admin): add onScroll handler to Editor and improve PostEditor layout

--- components/admin/Editor.tsx
+++ components/admin/Editor.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import 'react-markdown-editor-lite/lib/index.css';
 import MarkdownIt from 'markdown-it';
 import dynamic from 'next/dynamic';
-import { MDXEditor, codeBlockPlugin, headingsPlugin, listsPlugin, linkPlugin, quotePlugin, markdownShortcutPlugin, useCodeBlockEditorContext, toolbarPlugin, KitchenSinkToolbar, linkDialogPlugin, AdmonitionDirectiveDescriptor, codeMirrorPlugin, diffSourcePlugin, directivesPlugin, frontmatterPlugin, imagePlugin, tablePlugin, thematicBreakPlugin, DiffSourceToggleWrapper } from '@mdxeditor/editor';
+// import { MDXEditor, codeBlockPlugin, headingsPlugin, listsPlugin, linkPlugin, quotePlugin, markdownShortcutPlugin, useCodeBlockEditorContext, toolbarPlugin, KitchenSinkToolbar, linkDialogPlugin, AdmonitionDirectiveDescriptor, codeMirrorPlugin, diffSourcePlugin, directivesPlugin, frontmatterPlugin, imagePlugin, tablePlugin, thematicBreakPlugin, DiffSourceToggleWrapper } from '@mdxeditor/editor';
 
 // Dynamically import the Markdown editor to avoid SSR issues
 const MdEditor = dynamic(() => import('react-markdown-editor-lite'), { ssr: false });
@@ -16,10 +16,11 @@ interface EditorProps {
     markdown: string;
     onChange: (markdown: string) => void;
     parseMarkdown?: (markdown: string) => string;
+    onScroll?: (e: React.UIEvent<HTMLTextAreaElement | HTMLDivElement>, type: 'md' | 'html') => void;
 }
 
 
-const Editor = ({ markdown, onChange, parseMarkdown } : EditorProps) => {
+const Editor = ({ markdown, onChange, parseMarkdown , onScroll} : EditorProps) => {
     return (
         
         // <MDXEditor
@@ -47,8 +48,9 @@ const Editor = ({ markdown, onChange, parseMarkdown } : EditorProps) => {
 
         // lite editor
         <MdEditor
+            onScroll={onScroll}
             style={{ height: "500px" }}
-            renderHTML={(text) => mdParser.render(text)}
+            renderHTML={(text) => (parseMarkdown ? parseMarkdown(text) : mdParser.render(text))}
             onChange={({ text }) => onChange(text)}
             value={markdown}
         />
