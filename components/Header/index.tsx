3ed07625c047a06093eb2b76c90fb3b466ad28b0
Abdallah Saber <abdallahsaber065@gmail.com>
2024-10-26 23:18:01 +0300

    Refactor Header component to add admin dropdown menu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -13,6 +13,12 @@ const Header = () => {
   const [click, setClick] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
   const { data: session, status } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
 
   useEffect(() => {
     setIsMounted(true);
@@ -72,7 +78,7 @@ const Header = () => {
         <Link href="/contact" className="mx-2">
           Contact
         </Link>
-        {}
+        { }
         {isMounted && (
           <button
             onClick={() => setMode(mode === "light" ? "dark" : "light")}
@@ -101,9 +107,57 @@ const Header = () => {
         <span className="mx-2">|</span>
         {
           session?.user.role === "admin" && (
-            <Link href="/admin" className="mx-2">
-              Admin
-            </Link>
+            <div className="relative">
+              <button
+                id="dropdownDefaultButton"
+                data-dropdown-toggle="dropdown"
+                onClick={toggleDropdown}
+                className="text-dark dark:text-light bg-light dark:bg-dark hover:bg-gray-200 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:focus:ring-gray-800 font-medium rounded-lg text-sm px-5  text-center inline-flex items-center"
+                type="button"
+              >
+                Admin
+                <svg
+                  className="w-2.5 h-2.5 ms-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+
+              {isOpen && (
+                <div
+                  id="dropdown"
+                  className="z-10 absolute mt-2 w-48 bg-light dark:bg-dark divide-y divide-gray-200 dark:divide-gray-700 rounded-lg shadow"
+                >
+                  <ul className="text-sm text-dark dark:text-light" aria-labelledby="dropdownDefaultButton">
+                    <li>
+                      <Link href="/admin/" className="block px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white">
+                        Dashboard
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/admin/create-post" className="block px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white">
+                        Create Post
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/admin/subscriptions" className="block px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white">
+                        Newsletter Subscriptions
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
           )
         }
         {isMounted && (
