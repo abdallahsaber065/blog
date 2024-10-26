3c216f9b9c9fa11e59b7bd854ee374f277f2b8d6
Abdallah Saber <abdallahsaber065@gmail.com>
2024-10-26 16:06:24 +0300

    Refactor redeploy script to remove old releases

--- redeploy.sh
+++ redeploy.sh
@@ -13,6 +13,13 @@ fi
 NEW_BUILD_DIR="releases/$(date +'%Y%m%d%H%M%S')"
 mv temp "$NEW_BUILD_DIR"
 
+# check the number of releases and keep only 2 latest releases
+RELEASES_COUNT=$(find releases -mindepth 1 -maxdepth 1 -type d | wc -l)
+if [ "$RELEASES_COUNT" -gt 3 ]; then
+    echo "Removing old releases"
+    find releases -mindepth 1 -maxdepth 1 -type d | sort | head -n -2 | xargs -I {} rm -rf {}
+fi
+
 ln -sfn "$NEW_BUILD_DIR" .next
 
 echo "Release $NEW_BUILD_DIR build successfully"
