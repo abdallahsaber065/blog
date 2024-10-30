695e7bb13b24391528333929870c5b3bd578d36a
Abdallah Saber <abdallahsaber065@gmail.com>
2024-10-30 04:54:53 +0300

    Refactor Editor component to enhance markdown rendering options and improve error handling in serializeContent API

--- pages/api/serializeContent.ts
+++ pages/api/serializeContent.ts
@@ -14,8 +14,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         try {
             const mdxSource = await serialize(content, Options as SerializeOptions);
             return res.status(200).json({ mdxSource });
-        } catch (error) {
-            return res.status(500).json({ error: 'Failed to serialize content' });
+        } catch (error : any) {
+            return res.status(500).json({ error: 'Failed to serialize content: ' + error.message });
         }
     } else {
         res.setHeader('Allow', ['POST']);
