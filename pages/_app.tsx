c37314453f013b0b6398178de3500ef6a6bf2428
Abdallah Saber <abdallahsaber065@gmail.com>
2024-11-07 06:32:38 +0200

    Integrate Google Analytics component and add tracking script to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 import RootLayout from './_layout';
 import { StrictMode } from 'react';
 import { SessionProvider } from 'next-auth/react';
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
@@ -9,6 +11,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     <SessionProvider session={session}>
       <RootLayout>
         <StrictMode>
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_MEASUREMENT_ID || 'default-ga-id'} />
           <Component {...pageProps} />
         </StrictMode>
       </RootLayout>
