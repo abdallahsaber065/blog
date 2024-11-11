af021741c7c3164ca0d2ce793c388fcc5ed16161
Abdallah Saber <abdallahsaber065@gmail.com>
2024-11-11 09:46:55 +0200

    Refactor BlogDetails component; update post prop type and enhance author display

--- components/Blog/BlogDetails.tsx
+++ components/Blog/BlogDetails.tsx
@@ -2,10 +2,10 @@ import { format, parseISO } from "date-fns";
 import Link from "next/link";
 import React from "react";
 import { slug } from "github-slugger";
-import { Post, Tag } from "@prisma/client";
+import { Tag } from "@prisma/client";
 
 interface BlogDetailsProps {
-  post: Post;
+  post: any;
   postSlug: string;
   tags: Tag[];
 }
@@ -13,16 +13,20 @@ interface BlogDetailsProps {
 const BlogDetails = ({ post, postSlug, tags }: BlogDetailsProps) => {
   return (
     <div className="px-2 md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg">
-      <time className="m-3">
+      <time className="mb-1 md:mb-0 md:mr-2 text-sm md:text-base font-bold">
         {post.published_at ? format(parseISO(post.published_at.toISOString()), "LLLL d, yyyy") : "Unpublished"}
       </time>
 
-      <div className="m-3">{post.reading_time} min read</div>
-      {tags.length > 0 && (
-        <Link href={`/categories/${slug(tags[0].name)}`} className="m-3">
-          #{tags[0].name}
+      <div className="flex items-center mb-1 md:mb-0 md:mr-2 text-sm md:text-base font-bold">
+        By: <Link href={`/authors/${slug(post.author.username)}`} className="ml-1 text-accentDark dark:text-accent hover:underline">
+          @{post.author.username}
         </Link>
-      )}
+      </div>
+
+      <div className="mb-1 md:mb-0 md:mr-2 text-sm md:text-base font-bold">
+        {post.reading_time} min read
+      </div>
+
     </div>
   );
 };
