a5485b9afa041e529c15ae8f95fd59e32cbee29d
Abdallah Saber <abdallahsaber065@gmail.com>
2024-11-12 05:49:47 +0200

    Refactor IP address retrieval in rate limiting middleware; streamline logic and add logging for better debugging

--- lib/applyRateLimit.ts
+++ lib/applyRateLimit.ts
@@ -11,18 +11,17 @@ interface ExtendedNextApiRequest extends NextApiRequest {
 export const applyRateLimit = async (req: ExtendedNextApiRequest, res: NextApiResponse, limiter: RequestHandler) => {
     // Manually set the request.ip property
     if (!req.ip) {
-        const ip = req.headers['x-forwarded-for']?.toString() || req.socket.remoteAddress || '';
-        if (!ip) {
-            try {
-                const response = await axios.get('https://api64.ipify.org?format=json');
-                req.ip = response.data.ip;
-            } catch (error) {
-                console.error('Failed to fetch IP address:', error);
-                req.ip = '';
-            }
-        } else {
-            req.ip = ip;
+
+        try {
+            const response = await axios.get('https://api64.ipify.org?format=json');
+            req.ip = response.data.ip;
+        } catch (error) {
+            console.error('Failed to fetch IP address:', error);
+            req.ip = '';
         }
+
+        console.log('req.ip', req.ip);
+
     }
 
     return new Promise((resolve, reject) => {
