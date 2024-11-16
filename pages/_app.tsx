6db47f7a0f1ef3f751374eb0867208c7eabb74a8
Abdallah Saber <abdallahsaber065@gmail.com>
2024-11-16 03:06:34 +0200

    feat(App, Polyfill): Add polyfill for Promise.withResolvers and integrate it into the application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,13 @@ import { StrictMode, useEffect, useState } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
 import { useRouter } from 'next/router';
+// pages/_app.js or pages/_app.jsx
+
+import { polyfillPromiseWithResolvers } from "@/utils/polyfilsResolver";
+
+import 'core-js/full/promise/with-resolvers.js';
+
+polyfillPromiseWithResolvers();
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter();
