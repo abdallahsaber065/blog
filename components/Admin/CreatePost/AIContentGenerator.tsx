28047932bef6e9c8579c36c665fafbd495e85fdc
Abdallah Saber <abdallahsaber065@gmail.com>
2024-12-04 17:58:41 +0200

    feat(AIContentGenerator): Update file URL handling in file selection and add folder prop to image selector

--- components/Admin/CreatePost/AIContentGenerator.tsx
+++ components/Admin/CreatePost/AIContentGenerator.tsx
@@ -102,7 +102,7 @@ const AIContentGenerator: React.FC<AIContentGeneratorProps> = ({
     const [showImageSelector, setShowImageSelector] = useState(false);
 
     const handleFileSelect = (file: FileProps) => {
-        const fileUrl = `https://collage.devtrend.tech/api/files/download?file_url_name=${file.file_name}`;
+        const fileUrl = `https://collage.devtrend.tech/api/files/download?file_url_name=${file.file_url.split('/').pop()}`;
         setSelectedFiles((prev) => [...prev, fileUrl]);
         onFileSelect([...selectedFiles, fileUrl]);
     };
@@ -271,6 +271,7 @@ const AIContentGenerator: React.FC<AIContentGeneratorProps> = ({
                     isOpen={showImageSelector}
                     onClose={() => setShowImageSelector(false)}
                     onSelect={handleImageSelect}
+                    folder='blog'
                 />
             )}
         </div>
