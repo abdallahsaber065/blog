ffdb3ce33a137430019f9cb4a1b3d740570173ca
Abdallah Saber <abdallahsaber065@gmail.com>
2024-12-07 04:07:04 +0200

    feat(Embed): Enhance Embed component with responsive sizing, loading state, and support for YouTube and Google Drive links

--- components/MdxComponents/Embed/Embed.tsx
+++ components/MdxComponents/Embed/Embed.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { TbPlugConnectedX } from "react-icons/tb";
+// loading icon from react-icons
+import { FaSpinner } from "react-icons/fa";
 
-// Accept any additional props using a generic type
 interface EmbedProps extends React.IframeHTMLAttributes<HTMLIFrameElement> {
     src: string;
     title?: string;
@@ -12,32 +14,84 @@ interface EmbedProps extends React.IframeHTMLAttributes<HTMLIFrameElement> {
 const Embed: React.FC<EmbedProps> = ({
     src,
     title = 'Embedded content',
-    width = '400',
-    height = '500',
+    width = '100%',
+    height = '350',
     className = '',
-    ...props // Spread operator to collect all other props
+    ...props
 }) => {
     const [isClient, setIsClient] = useState(false);
+    if (height === 'auto' || height === '100%' || height === '') {
+        height = '350';
+    }
+    const defWidth = (width === 'auto' || width === '100%' || width === '')
 
     useEffect(() => {
         setIsClient(true);
     }, []);
 
+    const containerStyle = {
+        width: defWidth ? '100%' : width,
+        height: height,
+        maxWidth: '100%',
+    };
+
+    // use regex to check if youtube link is valid embed and if nou construct the embed link
+    if (src && src.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([a-zA-Z0-9_-]+)/)) {
+        src = src.replace(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([a-zA-Z0-9_-]+)/, "https://www.youtube.com/embed/$1");
+    }
+
+    // if google drive folder:convert to https://drive.google.com/embeddedfolderview?id=FOLDER-ID#list
+    if (src && src.match(/(?:https?:\/\/)?(?:drive\.google\.com)\/(?:drive\/folders)\/([a-zA-Z0-9_-]+)/)) {
+        src = src.replace(/(?:https?:\/\/)?(?:drive\.google\.com)\/(?:drive\/folders)\/([a-zA-Z0-9_-]+)/, "https://drive.google.com/embeddedfolderview?id=$1#list");
+    }
+
+    // if google drive file: convert to https://drive.google.com/file/d/FILE-ID/preview
+    if (src && src.match(/(?:https?:\/\/)?(?:drive\.google\.com)\/(?:file)\/d\/([a-zA-Z0-9_-]+)(?:\/[^?]*)?/)) {
+        src = src.replace(/(?:https?:\/\/)?(?:drive\.google\.com)\/(?:file)\/d\/([a-zA-Z0-9_-]+)(?:\/[^?]*)?/, "https://drive.google.com/file/d/$1/preview");
+    }
+
     if (!isClient) {
-        return <div className={`embed-container ${className}`}>Loading...</div>;
+        return (
+            <div className={`embed-container ${className}`} style={containerStyle}>
+                    <FaSpinner
+                        title={title}
+                        width={width}
+                        height={height}
+                        style={{
+                            width: defWidth ? '100%' : width,
+                            maxWidth: '100%',
+                        }}
+                    />
+            </div>
+
+        )
+    }
+
+    if (!src) {
+        return (
+            <div className={`embed-container ${className}`} style={containerStyle}>
+                <TbPlugConnectedX />
+                <p>Please insert a valid src</p>
+            </div>
+        );
     }
 
     return (
-        <div className={`embed-container ${className}`}>
+        <div className={`embed-container ${className}`} style={containerStyle}>
             <iframe
                 src={src}
                 title={title}
                 width={width}
                 height={height}
+                className="embed-iframe"
                 loading="lazy"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;"
                 allowFullScreen
-                {...props} // Spread all additional props to the iframe element
+                {...props}
+                style={{
+                    width: defWidth ? '100%' : width,
+                    maxWidth: '100%',
+                }}
             />
         </div>
     );
