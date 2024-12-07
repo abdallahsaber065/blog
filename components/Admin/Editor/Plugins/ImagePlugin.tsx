79254394b0b31dce6e8783a12d98c111cc316fdc
Abdallah Saber <abdallahsaber065@gmail.com>
2024-12-07 04:43:36 +0200

    feat(ImagePlugin): Add image selector for inserting images and manage its open state

--- components/Admin/Editor/Plugins/ImagePlugin.tsx
+++ components/Admin/Editor/Plugins/ImagePlugin.tsx
@@ -1,41 +1,66 @@
 import * as React from "react";
-// image icon from react-icons
 import { IoImage } from "react-icons/io5";
-// Using type assertion for the dynamic import
+import ImageSelector from "../../ImageSelector"; // Adjust the import path as necessary
+
+interface ImagePluginState {
+    isImageSelectorOpen: boolean;
+}
+
 export default class ImagePlugin extends (await import('react-markdown-editor-lite')).PluginComponent {
     static pluginName: string = "ImagePlugin";
     static align: "left" | "right" = "left";
+
+    state: ImagePluginState;
 
     constructor(props: any) {
         super(props);
 
         this.handleClick = this.handleClick.bind(this);
+        this.handleImageSelect = this.handleImageSelect.bind(this);
+        this.closeImageSelector = this.closeImageSelector.bind(this);
 
         this.state = {
-            num: this.getConfig("start")
+            isImageSelectorOpen: false,
         };
     }
 
     handleClick(): void {
-        this.editor.insertText("<Image src='' alt='' />");
+        this.setState({ isImageSelectorOpen: true });
+    }
+
+    handleImageSelect(image: any): void {
+        this.editor.insertText(`<Image src='${image.file_url}' alt='${image.file_name}' />`);
+        this.setState({ isImageSelectorOpen: false });
+    }
+
+    closeImageSelector(): void {
+        this.setState({ isImageSelectorOpen: false });
     }
 
     render(): React.ReactElement {
         return (
-            <span
-                className="button button-type-counter"
-                title="Insert Image"
-                onClick={this.handleClick}
-                style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-
-            >
-                <IoImage
-                    style={{
-                        height: '18.5px',
-                        width: '18px'
-                    }}
-                />
-            </span>
+            <>
+                <span
+                    className="button button-type-counter"
+                    title="Insert Image"
+                    onClick={this.handleClick}
+                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                >
+                    <IoImage
+                        style={{
+                            height: '18.5px',
+                            width: '18px'
+                        }}
+                    />
+                </span>
+                {this.state.isImageSelectorOpen && (
+                    <ImageSelector
+                        isOpen={this.state.isImageSelectorOpen}
+                        onClose={this.closeImageSelector}
+                        onSelect={this.handleImageSelect}
+                    />
+                )}
+            </>
         );
     }
 }
